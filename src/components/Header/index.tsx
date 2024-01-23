5ebb43007ce8944ee7b77afd4da21d8aa580dcd6
Ian-Bright <ianbrighton1@gmail.com>
2024-01-23 16:38:28 -0700

    feat: Base tailwind configuartion added and header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,23 @@
+import discord from 'assets/images/discord.svg';
+import github from 'assets/images/github.svg';
+import logo from 'assets/images/logo.svg';
+
 export default function Header(): JSX.Element {
-  return <div className='flex h-[120px] items-center px-16'>Test</div>;
+  return (
+    <div className='flex h-[120px] items-center justify-between px-4 md:px-16'>
+      <img alt='Logo' src={logo} />
+      <div className='flex gap-4 shrink-0'>
+        <a href='https://discord.com' rel='noreferrer' target='_blank'>
+          <img alt='Discord' src={discord} />
+        </a>
+        <a
+          href='https://github.com/SemaphoreNetwork'
+          rel='noreferrer'
+          target='_blank'
+        >
+          <img alt='Github' src={github} />
+        </a>
+      </div>
+    </div>
+  );
 }
